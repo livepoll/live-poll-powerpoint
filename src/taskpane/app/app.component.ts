3d544c1eff9af1718f2bd48c68f4348f6cbda4ac
Marc Auberer <marc.auberer@chillibits.com>
2020-12-19 00:34:22 +0100

    Add basic UI to taskpane

--- src/taskpane/app/app.component.ts
+++ src/taskpane/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+
 // images references in the manifest
 import "../../../assets/icon-16.png";
 import "../../../assets/icon-32.png";
 import "../../../assets/icon-80.png";
+
 const template = require("./app.component.html");
 /* global console, Office, require */
 
@@ -11,21 +13,22 @@ const template = require("./app.component.html");
   template
 })
 export default class AppComponent {
-  welcomeMessage = "Welcome";
 
-  async run() {
-    /**
-     * Insert your PowerPoint code here
-     */
+  loading = false;
+
+  /**
+   * Takes the entered url, and inserts an element with a live view of the poll results to the current slide
+   */
+  async insertPoll() {
+    this.loading = true;
+    // Add element to slide
     Office.context.document.setSelectedDataAsync(
       "Hello World!",
       {
         coercionType: Office.CoercionType.Text
       },
       result => {
-        if (result.status === Office.AsyncResultStatus.Failed) {
-          console.error(result.error.message);
-        }
+        if (result.status === Office.AsyncResultStatus.Failed) console.error(result.error.message);
       }
     );
   }
